fc4387d5d9712a2c462551f48ccbe2feff90f859
Ledanis <daniel.puentes.n@gmail.com>
2025-09-11 10:55:54 -0300

    Se añaden columnas en Botones Dinamicos.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,11 +14,24 @@
 			<span>O</span>
 			<span>U</span>
 			<span>T</span>
+			<br />
+			<span>M</span>
+			<span>E</span>
 		</div>
 	</div>
-	<p class="Point-2">
+	<div class="Point-2">
 		<FontAwesomeIcon icon={faBook} size="2x" />
-	</p>
+		<div class="about2">
+			<span>K</span>
+			<span>N</span>
+			<span>O</span>
+			<span>L</span>
+			<span>E</span>
+			<span>D</span>
+			<span>G</span>
+			<span>E</span>
+		</div>
+	</div>
 	<p class="Point-3">
 		<FontAwesomeIcon icon={faDiagramProject} size="2x" />
 	</p>
@@ -74,7 +87,7 @@
 			transform 0.3s ease;
 		color: white;
 		font-weight: bold;
-		font-size: 25px;
+		font-size: 15px;
 		font-family: 'Kode Mono', system-ui, sans-serif;
 	}
 
@@ -100,21 +113,38 @@
 	.about span:nth-child(5) {
 		transition-delay: 0.45s;
 	}
-
+	.about span:nth-child(6) {
+		transition-delay: 0.55s;
+	}
+	.about span:nth-child(7) {
+		transition-delay: 0.65s;
+	}
+	.about span:nth-child(8) {
+		transition-delay: 0.75s;
+	}
 	/* Desaparición inversa */
 	.Point:not(:hover) .about span:nth-child(1) {
-		transition-delay: 0.45s;
+		transition-delay: 0.65s;
 	}
 	.Point:not(:hover) .about span:nth-child(2) {
-		transition-delay: 0.35s;
+		transition-delay: 0.45s;
 	}
 	.Point:not(:hover) .about span:nth-child(3) {
-		transition-delay: 0.25s;
+		transition-delay: 0.35s;
 	}
 	.Point:not(:hover) .about span:nth-child(4) {
-		transition-delay: 0.15s;
+		transition-delay: 0.25s;
 	}
 	.Point:not(:hover) .about span:nth-child(5) {
+		transition-delay: 0.15s;
+	}
+	.Point:not(:hover) .about span:nth-child(6) {
+		transition-delay: 0.1s;
+	}
+	.Point:not(:hover) .about span:nth-child(7) {
+		transition-delay: 0.05s;
+	}
+	.Point:not(:hover) .about span:nth-child(8) {
 		transition-delay: 0.05s;
 	}
 
@@ -136,6 +166,88 @@
 
 	.Point-2:hover {
 		transform: scale(1.2); /* aumenta un 20% */
+	}
+
+	/* Contenedor de letras debajo en columna */
+	.about2 {
+		position: absolute;
+		top: 110%; /* debajo del cubo */
+		left: 50%;
+		transform: translateX(-50%);
+		display: flex;
+		flex-direction: column; /* vertical */
+		align-items: center;
+		gap: 5px;
+	}
+
+	.about2 span {
+		opacity: 0;
+		transform: translateY(10px);
+		transition:
+			opacity 0.3s ease,
+			transform 0.3s ease;
+		color: white;
+		font-weight: bold;
+		font-size: 15px;
+		font-family: 'Kode Mono', system-ui, sans-serif;
+	}
+
+	/* Al hacer hover aparecen */
+	.Point-2:hover .about2 span {
+		opacity: 1;
+		transform: translateY(0);
+	}
+
+	/* Aparición escalonada */
+	.about2 span:nth-child(1) {
+		transition-delay: 0.05s;
+	}
+	.about2 span:nth-child(2) {
+		transition-delay: 0.15s;
+	}
+	.about2 span:nth-child(3) {
+		transition-delay: 0.25s;
+	}
+	.about2 span:nth-child(4) {
+		transition-delay: 0.35s;
+	}
+	.about2 span:nth-child(5) {
+		transition-delay: 0.45s;
+	}
+	.about2 span:nth-child(6) {
+		transition-delay: 0.55s;
+	}
+	.about2 span:nth-child(7) {
+		transition-delay: 0.65s;
+	}
+	.about2 span:nth-child(8) {
+		transition-delay: 0.75s;
+	}
+
+	/* Desaparición inversa */
+	.Point-2:not(:hover) .about2 span:nth-child(1) {
+		transition-delay: 0.75s;
+	}
+	.Point-2:not(:hover) .about2 span:nth-child(2) {
+		transition-delay: 0.65s;
+	}
+	.Point-2:not(:hover) .about2 span:nth-child(3) {
+		transition-delay: 0.55s;
+	}
+	.Point-2:not(:hover) .about2 span:nth-child(4) {
+		transition-delay: 0.45s;
+	}
+	.Point-2:not(:hover) .about2 span:nth-child(5) {
+		transition-delay: 0.35s;
+	}
+	.Point-2:not(:hover) .about2 span:nth-child(6) {
+		transition-delay: 0.25s;
+	}
+	.Point-2:not(:hover) .about2 span:nth-child(7) {
+		transition-delay: 0.15s;
+	}
+	.Point-2:not(:hover) .about2 span:nth-child(8) {
+		transition-delay: 0.05s;
 	}
 
 	.Point-3 {
