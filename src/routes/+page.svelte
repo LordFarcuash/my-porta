cfc0f187192e8af57b3ed429cf34b93f41168b02
Ledanis <daniel.puentes.n@gmail.com>
2025-09-11 17:38:56 -0300

    Se agrega Titulo Name y SubTitle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,12 @@
 
 <div class="navbar">
 	<ButtonsNav />
+	<div class="TitleName">
+		<span>Ledanis</span>
+	</div>
+	<div class="SubTitle">
+		<span>Full-Stack Developer</span>
+	</div>
 </div>
 
 <style>
@@ -14,5 +20,29 @@
 		border-right: 100vh solid transparent; /* otro lado */
 		border-top: 50vh solid #3e413b; /* color del navbar */
 		filter: drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.4));
+	}
+
+	.TitleName span {
+		font-family: 'Kode Mono', system-ui, sans-serif;
+		font-size: 5rem;
+		color: white;
+	}
+
+	.TitleName {
+		position: absolute;
+		top: -36rem;
+		left: 28rem;
+	}
+
+	.SubTitle {
+		width: 20rem;
+		position: absolute;
+		top: -30rem;
+		left: 29rem;
+	}
+	.SubTitle span {
+		font-family: 'Kode Mono', system-ui, sans-serif;
+		font-size: 1rem;
+		color: white;
 	}
 </style>
